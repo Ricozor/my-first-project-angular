edcaf7df8acbcf6ec3b3dc5347708d11734640e6
Eric Fressange Dubost <efressange-dubost@niji.fr>
2022-03-08 11:12:55 +0100

    Create header and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AddRecipeComponent } from './pages/add-recipe/add-recipe.component';
+import { HeaderComponent } from './components/organisms/header/header.component';
+import { FooterComponent } from './components/organisms/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AddRecipeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
