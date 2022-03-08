91bbe196621a7367ca072cd747560b21d674ba86
Eric Fressange Dubost <efressange-dubost@niji.fr>
2022-03-08 11:04:08 +0100

    Create pages Home and Add Recipe + create routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AddRecipeComponent } from './pages/add-recipe/add-recipe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'add-recipe', component: AddRecipeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
